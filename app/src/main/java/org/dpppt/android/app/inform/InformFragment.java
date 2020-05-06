a6fa24fd063aba603931534815f6ac43e24d38cc
Simon Rösch <roesch@ubique.ch>
2020-05-06 21:51:24 +0200

    remove unused networking stuff and fix proguard rules

--- app/src/main/java/org/dpppt/android/app/inform/InformFragment.java
+++ app/src/main/java/org/dpppt/android/app/inform/InformFragment.java
@@ -22,11 +22,6 @@ import java.util.Date;
 
 import org.dpppt.android.app.R;
 import org.dpppt.android.app.inform.views.ChainedEditText;
-import org.dpppt.android.app.networking.AuthCodeRepository;
-import org.dpppt.android.app.networking.errors.InvalidCodeError;
-import org.dpppt.android.app.networking.errors.ResponseError;
-import org.dpppt.android.app.networking.models.AuthenticationCodeRequestModel;
-import org.dpppt.android.app.networking.models.AuthenticationCodeResponseModel;
 import org.dpppt.android.app.storage.SecureStorage;
 import org.dpppt.android.app.util.InfoDialog;
 import org.dpppt.android.sdk.DP3T;
@@ -91,17 +86,12 @@ public class InformFragment extends Fragment {
 
 		buttonSend.setOnClickListener(v -> {
 			buttonSend.setEnabled(false);
-			setInvalidCodeErrorVisible(false);
 			String authCode = authCodeInput.getText();
 
 			progressDialog = createProgressDialog();
-			if (System.currentTimeMillis() - lastRequestTime < TIMEOUT_VALID_CODE && lastToken != null) {
-				Calendar calendar = Calendar.getInstance();
-				calendar.add(Calendar.MONTH, -14);
-				informExposed(calendar.getTime(), getAuthorizationHeader(lastToken));
-			} else {
-				authenticateInput(authCode);
-			}
+			Calendar calendar = Calendar.getInstance();
+			calendar.add(Calendar.DATE, -14);
+			informExposed(calendar.getTime(), authCode);
 		});
 
 		view.findViewById(R.id.cancel_button).setOnClickListener(v -> {
@@ -109,42 +99,9 @@ public class InformFragment extends Fragment {
 		});
 	}
 
-	private void authenticateInput(String authCode) {
-		AuthCodeRepository authCodeRepository = new AuthCodeRepository(getContext());
-		authCodeRepository.getAccessToken(new AuthenticationCodeRequestModel(authCode, 0),
-				new ResponseCallback<AuthenticationCodeResponseModel>() {
-					@Override
-					public void onSuccess(AuthenticationCodeResponseModel response) {
-						String accessToken = response.getAccessToken();
-
-						secureStorage.saveInformTimeAndCodeAndToken(authCode, accessToken);
-						Calendar calendar = Calendar.getInstance();
-						calendar.add(Calendar.MONTH, -14);
-						informExposed(calendar.getTime(), getAuthorizationHeader(accessToken));
-					}
-
-					@Override
-					public void onError(Throwable throwable) {
-						if (progressDialog != null && progressDialog.isShowing()) {
-							progressDialog.dismiss();
-						}
-						if (throwable instanceof InvalidCodeError) {
-							setInvalidCodeErrorVisible(true);
-							return;
-						} else if (throwable instanceof ResponseError) {
-							showErrorDialog(getString(R.string.unexpected_error_title),
-									String.valueOf(((ResponseError) throwable).getStatusCode()));
-						} else {
-							showErrorDialog(getString(R.string.network_error), null);
-						}
-						buttonSend.setEnabled(true);
-					}
-				});
-	}
-
-	private void informExposed(Date onsetDate, String authorizationHeader) {
+	private void informExposed(Date onsetDate, String authCode) {
 		DP3T.sendIAmInfected(getContext(), onsetDate,
-				new ExposeeAuthMethodJson(authorizationHeader), new ResponseCallback<Void>() {
+				new ExposeeAuthMethodJson(authCode), new ResponseCallback<Void>() {
 					@Override
 					public void onSuccess(Void response) {
 						if (progressDialog != null && progressDialog.isShowing()) {
@@ -176,11 +133,6 @@ public class InformFragment extends Fragment {
 		authCodeInput.requestFocus();
 	}
 
-	private void setInvalidCodeErrorVisible(boolean visible) {
-		getView().findViewById(R.id.inform_invalid_code_error).setVisibility(visible ? View.VISIBLE : View.GONE);
-		getView().findViewById(R.id.inform_input_text).setVisibility(visible ? View.GONE : View.VISIBLE);
-	}
-
 	private AlertDialog createProgressDialog() {
 		return new AlertDialog.Builder(getContext())
 				.setView(R.layout.dialog_loading)
@@ -189,10 +141,6 @@ public class InformFragment extends Fragment {
 
 	private void showErrorDialog(String error, @Nullable String errorCode) {
 		InfoDialog.newInstanceWithDetail(error, errorCode).show(getChildFragmentManager(), InfoDialog.class.getCanonicalName());
-	}
-
-	private String getAuthorizationHeader(String accessToken) {
-		return "Bearer " + accessToken;
 	}
 
 }
